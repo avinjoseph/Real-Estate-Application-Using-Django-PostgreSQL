f7986424cc611a3ef7585e456a329c616d11a774
Avin Joseph <avin.chelseafc@gmail.com>
2022-10-10 08:35:39 +0530

    Accounts and Authentication added

--- btre/settings.py
+++ btre/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'listings.apps.ListingsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
@@ -137,3 +138,9 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
